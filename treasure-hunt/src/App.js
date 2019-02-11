8f2aafc7951a8003892a5f9bce52758a9fe738d3
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-11 14:59:30 -0800

    created Traversal class component

---
+++ treasure-hunt/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import Traversal from './components/traversal'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Traversal />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ treasure-hunt/src/components/traversal.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Traversal extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return <h1>hello</h1>
+  }
+}
