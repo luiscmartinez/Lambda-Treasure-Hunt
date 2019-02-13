1a45df97e3d0bedce2bd8d98d38db49b8960fb82
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-12 20:38:39 -0800

    implemented logic to store graph to local storage and refactored Traversal component

--- treasure-hunt/src/components/traversal.js
+++ treasure-hunt/src/components/traversal.js
@@ -8,26 +8,105 @@ axios.defaults.headers.post['Content-Type'] = 'application/json'
 export default class Traversal extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      coordinates: {},
+      graph: {},
+      room_id: 0,
+      cooldown: 0
+    }
   }
+
   componentDidMount () {
+    if (localStorage.hasOwnProperty('graph')) {
+      const graph = JSON.parse(localStorage.getItem('graph'))
+      this.setState({ graph })
+    }
     axios
       .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/')
-      .then((response) => {
-        console.log(response.data)
+      .then((res) => {
+        console.log(res.data)
+        const { room_id, coordinates, exits, cooldown } = res.data
+        this.setState({ room_id, cooldown })
+        const prev_room_id = this.state.room_id
+        console.log(this.state.room_id)
+        const graph = this.updateGraph(
+          room_id,
+          this.parseCoordinates(coordinates),
+          exits,
+          prev_room_id
+        )
+        this.setState({ graph })
       })
       .catch((error) => {
-        console.log(error.response.data)
+        console.error(error.response.data)
       })
   }
-  handleMovement (direction) {
+
+  handleMovement = (move) => {
     axios
       .post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', {
-        direction: direction
+        direction: move
       })
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        const { room_id, coordinates, exits, cooldown } = res.data
+        const prev_room_id = this.state.room_id
+        const graph = this.updateGraph(
+          room_id,
+          this.parseCoordinates(coordinates),
+          exits,
+          prev_room_id,
+          move
+        )
+        this.setState({
+          room_id,
+          coordinates: this.parseCoordinates(coordinates),
+          cooldown,
+          graph
+        })
+        console.log(this.state.graph)
+        console.log(res.data)
+      })
       .catch((err) => console.error(err))
   }
+
+  updateGraph = (id, coordinates, exits, prev_room_id = null, move = null) => {
+    let graph = Object.assign({}, this.state.graph)
+    if (!this.state.graph[id]) {
+      const payload = {}
+      payload['coordinates'] = coordinates
+      const moves = {}
+      for (let exit of exits) {
+        moves[exit] = '?'
+      }
+      payload['exits'] = moves
+      graph = { ...graph, [id]: payload }
+    }
+    if (prev_room_id && move) {
+      console.log(graph[prev_room_id]['exits'][move])
+      const inverseDirection = this.get_inverseDirection(move)
+      graph[prev_room_id]['exits'][move] = id
+      graph[id]['exits'][inverseDirection] = prev_room_id
+    }
+    localStorage.setItem('graph', JSON.stringify(graph))
+    return graph
+  }
+  get_inverseDirection = (move) => {
+    const inverseDir = { n: 's', s: 'n', w: 'e', e: 'w' }
+    return inverseDir[move]
+  }
+  parseCoordinates = (coordinates) => {
+    const x = coordinates.replace(/[%^()]/g, '').split(',')[0]
+    const y = coordinates.replace(/[%^()]/g, '').split(',')[1]
+    const coordsObject = { x, y }
+    return coordsObject
+  }
+
   render () {
-    return <h1>hello</h1>
+    return (
+      <div>
+        <h1>hello</h1>
+        <h2 onClick={() => this.handleMovement('n')}>move</h2>
+      </div>
+    )
   }
 }
